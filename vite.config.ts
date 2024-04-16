79aecb8a97a1948468f6018d46559dba682aa296
sutthinai <sutthinai.byyc@gmail.com>
2024-04-16 16:50:22 +0700

    build: add vite-plugin-cross-origin-isolation to fix SharedArrayBuffer in Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), crossOriginIsolation()],
   optimizeDeps: {
     exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
   },
@@ -13,5 +14,5 @@ export default defineConfig({
       "Cross-Origin-Embedder-Policy": "require-corp",
     },
   },
-  base: "/audio-converter/",
+  base: "/audio-converter-online/",
 });
